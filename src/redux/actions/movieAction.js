e7741f8d522a5a6801f38758d4239c86ce137aa0
JINBOK LEE <eyelash1024@naver.com>
2022-07-19 20:15:22 +0900

    api call and promise.all modified

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,10 +1,29 @@
 import api from "../api";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = await api.get(
-      `/movie/popular?api_key=a25b654a5fefe228544dc5b8b1712130&language=en-US&page=1`
+    const popularMovieApi = api.get(
+      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
+
+    const topRatedApi = api.get(
+      `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    const upComingApi = api.get(
+      `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+      popularMovieApi,
+      topRatedApi,
+      upComingApi,
+    ]);
+    console.log("promise all 이후의 popularMovies 데이터는?", popularMovies);
+    console.log("promise all 이후의 topRatedMovies 데이터는?", topRatedMovies);
+    console.log("promise all 이후의 upComingMovies 데이터는?", upComingMovies);
   };
 }
 
